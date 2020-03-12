e7e37b85e726b8f5f91d5685cf71198d47d708ef
vladwdw <57443656+vladwdw@users.noreply.github.com>
2020-03-12 16:20:50 +0300

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,20 @@
+# Установка!:
+
+* apt update && apt upgrade
+* pkg install git
+* git clone https://github.com/Artem450-ss/NanoBomber
+* sh NanoBomber/install.sh
+* Готово! 
+
+# Обновление бомбера!
+* rm -rf NanoBomber
+* git clone https://github.com/Artem450-ss/NanoBomber
+* cd NanoBomber
+* python3 nano.py
+
+# Инфо
+если уж что-то не понятно, пиши в телеграм разработчику! @artem450
+
+# Канал
+
+* У бомбера есть свой Telegram-канал😊😎 @nanobomber
---
+++ install.sh
@@ -0,0 +1,35 @@
+﻿clear
+echo "-----------------------------"
+echo "| Куда устанавливать будем? |"
+echo "|---------------------------|"
+echo "| 1. Termux                 |"
+echo "| 2. iSH                    |"
+echo "|                           |"
+echo "| made by @vlad_wdw        |"
+echo "| Введите 1 или 2:          |"
+echo "-----------------------------"
+read numb
+if [ $numb = "1" ]
+then
+      apt install python
+      apt install python3
+      apt install python3-pip
+      pip3 install requests
+      pip3 install colorama
+      cd bomb
+er      python3 bomber.py
+else
+      if [ $numb = "2" ]
+      then
+            apk add python
+            apk add python3
+            apk add python3-pip
+            pip3 install requests
+            pip3 install colorama
+            cd bomber
+            python3 bomber.py
+      else
+      echo "Некорректный ввод"
+fi
+fi
+            
